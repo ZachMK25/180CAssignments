b735018e82bc3b553b832841b467f3045e6a4a64
Zach Kuo <k.zach@wustl.edu>
2025-02-13 03:14:59 +0000

    swapped validation to getopt, more robust

--- Assignment1/lottery.c
+++ Assignment1/lottery.c
@@ -3,17 +3,20 @@
 #include <ctype.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>  
+
 
 // https://stackoverflow.com/questions/3219393/stdlib-and-colored-output-in-c
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
 
 
 // https://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Randomization.html
 // how to generate random numbers
 
 int errorIncorrectUsage(){
-    printf("Usage: ./lottery -n [Numbers to Generate] -r [Max Number] (optional: -p [Max Powerball Number]) -N [Numbers of Sets to Generate]\n");
+    printf("\nUsage: ./lottery -n [Numbers to Generate, int >= 1] -r [Max Number, int >= 1] (optional: -p [Max Powerball Number, int >= 0]) -N [Numbers of Sets to Generate, int >= 1]\n");
     return 1;
 }
 
@@ -21,51 +24,70 @@ int validateOptions(int argc, char** argv, int* n, int* r, int* p, int* N){
 
     // input validation
     
-    // must have either 7 or 9 arguments, depending on if p is included
-    if (!(argc == 7 || argc == 9)){
-        return errorIncorrectUsage();
-    }
 
-    int i = 0;
-    for (i = 1; i < argc - 1; i+=2) {
+    int opt;
+    int passed;
+    
+    while((opt = getopt(argc, argv, "n:N:r:p:")) != -1){
 
-        // stop the program before reading an index that wouldn't be defined
-        if (((int)strlen(argv[i]) != 2) || !isalpha(argv[i][1])){
-            return errorIncorrectUsage();
-        }
+        // USE GETOPTIONS INSTEAD
 
-        char option = argv[i][1];
-        long int passed=0;
-        char * ptrend;
-        long int test;
-
-        switch(option){
+        switch(opt){
             case 'n':
-                passed = strtol(argv[i+1], &ptrend, 10);
+                passed = atoi(optarg);
+                // enforce integer
+                if (passed == 0 && optarg[0] != '0'){
+                    fprintf(stderr, "Error: -n requires a valid integer argument.\n");
+                }
+
                 *n = passed;
+                // printf("%c = %d\n", opt, *n);
+
                 break;
 
             case 'r':
-                passed = strtol(argv[i+1], &ptrend, 10);
+                passed = atoi(optarg);
+                // enforce integer
+                if (passed == 0 && optarg[0] != '0'){
+                    fprintf(stderr, "Error: -r requires a valid integer argument.\n");
+                }
                 *r = passed;
+                // printf("%c = %d\n", opt, *r);
+
                 break;
 
             case 'p':
-                passed = strtol(argv[i+1], &ptrend, 10);
+                passed = atoi(optarg);
+                // enforce integer
+                if (passed == 0 && optarg[0] != '0'){
+                    fprintf(stderr, "Error: -p requires a valid integer argument.\n");
+                }
                 *p = passed;
+                // printf("%c = %d\n", opt, *p);
                 break;
 
             case 'N':
-                passed = strtol(argv[i+1], &ptrend, 10);
+                passed = atoi(optarg);
+                // enforce integer
+                if (passed == 0 && optarg[0] != '0'){
+                    fprintf(stderr, "Error: -N requires a valid integer argument.\n");
+                }
                 *N = passed;
+                // printf("%c = %d\n", opt, *N);
                 break;
-            default:
-                printf("invalid option passed: -%c\n", option);
+
+            case '?':
                 return errorIncorrectUsage();
         }
     }
 
-    // printf("\noptions: \nn: %d\nr: %d \np: %d \nN: %d \n", *n,*r,*p,*N);
+    // optind is for the extra arguments 
+    // which are not parsed 
+    if (optind > argc){      
+        printf("more arguments passed than expected\n");
+        errorIncorrectUsage();
+    } 
+    
 
     if (*n <= 0 || *r <= 0 || *N <= 0 || *p<0){
         return errorIncorrectUsage();
@@ -93,6 +115,11 @@ int main(int argc, char** argv){
     int sets [N][n];
     int i;
 
+    if (p){
+        printf(ANSI_COLOR_MAGENTA "Powerball numbers are labeled in this color\n\n" ANSI_COLOR_RESET);
+    }
+
+
     for (i = 0; i < N; i++){
 
         int j;
@@ -108,9 +135,7 @@ int main(int argc, char** argv){
         else{
             sets[i][j] = (rand() % r) + 1;
         }
-
     }
-
 
     for (i = 0; i < N; i++){
         int j;
@@ -120,12 +145,11 @@ int main(int argc, char** argv){
 
         // label powerball column
         if (p != 0){
-            printf(ANSI_COLOR_RED "%d" ANSI_COLOR_RESET, sets[i][j]);
+            printf(ANSI_COLOR_MAGENTA "%d" ANSI_COLOR_RESET, sets[i][j]);
         }
         else{
             printf("%d", sets[i][j]);
         }
-
         printf("\n\n");   
     }
 
