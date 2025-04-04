d4693c0c421934afa2ea0a81d35396aee3edda4d
Zach Kuo <k.zach@wustl.edu>
2025-04-04 03:04:23 +0000

    finished

--- 5/pipe.c
+++ 5/pipe.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
 
-#define MSGSIZE 40
 #define READ 0
 #define WRITE 1
 
 int main(){
-    char inbuf[MSGSIZE];
+    // I am your daddy! and my name is <PID>
+    int MSGSIZE1 = 33 + 10;
+    int MSGSIZE2 = 19 + 10;
+
+    char inbuf1[MSGSIZE1];
+    char inbuf2[MSGSIZE2];
     int nbytes;
     int to_child[2];
     int to_parent[2];
@@ -24,28 +30,68 @@ int main(){
 
     if (pid > 0){
         // parent
-        const char* msg1 = "I am your daddy! and my name is parent\n";
 
-        write(to_child[WRITE], msg1, MSGSIZE);
+        char* msg1 = (char*)malloc(MSGSIZE1);
+        if (msg1 == NULL){
+            perror("malloc");
+            exit(ECHILD);
+        }
+
+        snprintf(msg1, MSGSIZE1, "I am your daddy! and my name is %d\n", getpid());
+
+        write(to_child[WRITE], msg1, MSGSIZE1);
+        close(to_child[WRITE]);
+
+        // reading the response from the child
+        close(to_parent[WRITE]);
+
+        while ((nbytes = read(to_parent[READ], inbuf2, MSGSIZE2)) > 0){
+            printf("%s", inbuf2);
+        }
+
+        close(to_parent[READ]);
+
+        if (nbytes != 0){
+            printf("Error: %s\n", strerror(EPIPE));
+            exit(EPIPE);
+        }
 
         int status;
         wait(&status);
-        printf("status: %d", status);
+        // printf("status: %d\n", status);
+
+        free(msg1);
+
         return 0;
     }
     else {
+        close(to_child[WRITE]);
         // child
 
-        do {
-            nbytes = read(to_child[READ], inbuf, MSGSIZE);
-            printf("%s", inbuf);
-        } while (nbytes >= 0);
+        while ((nbytes = read(to_child[READ], inbuf1, MSGSIZE1)) > 0){
+            printf("%s", inbuf1);
+        }
 
-        // while ((nbytes = read(to_child[READ], inbuf, MSGSIZE)) > 0)
-        printf("nbytes: %d", nbytes);
-        if (nbytes != 0)
-            exit(2);
-        printf("Finished reading\n");
+        close(to_child[READ]);
+
+        if (nbytes != 0){
+            printf("Error: %s\n", strerror(EPIPE));
+            exit(EPIPE);
+        }
+
+        // write response
+
+        char* msg2 = (char*)malloc(MSGSIZE2);
+        if (msg2 == NULL){
+            perror("malloc");
+            exit(ECHILD);
+        }
+
+        snprintf(msg2, MSGSIZE1, "Daddy, my name is %d\n", getpid());
+
+        write(to_parent[WRITE], msg2, MSGSIZE2);
+        close(to_parent[WRITE]);
+
         return 0;
     }
 
